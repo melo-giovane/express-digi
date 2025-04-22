c92b06ca7dda66fd48a1dae49a5ff519eb41e2aa
Giovane <giovanec.melo@hotmail.com>
2025-04-21 22:39:14 -0300

    chore: add .env file to .gitignore and update dependecies

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
@@ -6,7 +7,6 @@ app.use(express.json());
 
 let teaData = [];
 let nextId = 1;
-
 
 //add a new tea
 app.post("/teas", (req, res) => {
@@ -31,29 +31,27 @@ app.get("/teas/:id", (req, res) => {
 });
 
 //update tea
-
 app.put("/teas/:id", (req, res) => {
   const tea = teaData.find((t) => t.id === parseInt(req.params.id));
 
   if (!tea) {
     return res.status(404).send("tea not found");
   }
-  const {name, price} = req.body
-  tea.name = name
-  tea.price = price
-  res.send(200).send(tea)
+  const { name, price } = req.body;
+  tea.name = name;
+  tea.price = price;
+  res.send(200).send(tea);
 });
 
 //delete tea
-
-app.delete('/teas/:id', (req, res) => {
-  const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
-  if(index === -1) {
-    return res.status(404).send('tea not found')
+app.delete("/teas/:id", (req, res) => {
+  const index = teaData.findIndex((t) => t.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("tea not found");
   }
-  teaData.splice(index, 1)
-  return res.status(200).send('deleted')
-})
+  teaData.splice(index, 1);
+  return res.status(200).send("deleted");
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}...`);
